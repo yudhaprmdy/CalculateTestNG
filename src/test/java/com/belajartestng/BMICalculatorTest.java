5bb5add0ea86a28c6fe77948a627b30b1af48f6a
yudhaprmdy <indieyudha11@gmail.com>
2025-01-19 19:34:03 +0700

    Fix

--- src/test/java/com/belajartestng/BMICalculatorTest.java
+++ src/test/java/com/belajartestng/BMICalculatorTest.java
@@ -10,19 +10,19 @@ public class BMICalculatorTest {
     BMICalculator bmiCalculator = new BMICalculator();
     @Test
     public void testCalculateBMI_NormalWeight() {
-        double weight = 70; // dalam kg
-        double height = 1.75; // dalam meter
+        double weight = 70;
+        double height = 1.75;
 
         double expectedBMI = 22.86;
         double actualBMI = bmiCalculator.calculateBMI(weight, height);
 
-        // Toleransi sedikit perbedaan nilai floating point
+
         Assert.assertEquals(actualBMI, expectedBMI, 0.01);
     }
 
     @Test
     public void testBMICategory_NormalWeight() {
-        double bmi = 22.86; // BMI untuk normal weight
+        double bmi = 22.86;
         String expectedCategory = "Berat badan normal (Normal)";
         String actualCategory = bmiCalculator.categoryBMI(bmi);
 
